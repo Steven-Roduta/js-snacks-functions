1748b21025a475d50346d8cc046ec4318bc12e1b
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-05 17:30:54 +0100

    add group of names

--- ex-js-functions-snack-main/02 - Filtra iniziali/script.js
+++ ex-js-functions-snack-main/02 - Filtra iniziali/script.js
@@ -4,10 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function lettera(names){
+    return names.filter(nomi => nomi [0] === 'A' );
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let groupNames = lettera(names);
 
+console.log (groupNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
