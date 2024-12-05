94fb269e539d8b13341156b16c638d503228c69c
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-05 18:10:39 +0100

    add the vowels of the string

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -2,12 +2,16 @@
 
 const word = 'javascript';
 
+const vocali = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
-
+function vowels(word) {
+    return word.split('').filter(character => vocali.includes(character.toLowerCase()));
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+let jsVowel = vowels(word);
 
-
+console.log(jsVowel);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
